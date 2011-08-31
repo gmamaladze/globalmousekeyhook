7f66ea15a0ef194b65034008a4da77288cbfb61b
SND\gmamaladze_cp <SND\gmamaladze_cp@2abf6ff4-378a-4344-b6f7-2bcd8b316ba9>
2011-08-31 08:09:20 +0000

    Eliminating HookManager.Structures.cs. Moving all structure declarations each into separate .cs files.
    git-svn-id: https://globalmousekeyhook.svn.codeplex.com/svn@3251 2abf6ff4-378a-4344-b6f7-2bcd8b316ba9

---
+++ Gma.GlobalMouseKeyHooks/Gma.UserActivityMonitor/WinApi/MouseLLHookStruct.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Gma.UserActivityMonitor.WinApi
+{
+    /// <summary>
+    /// The MSLLHOOKSTRUCT structure contains information about a low-level keyboard input event. 
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MouseLLHookStruct {
+        /// <summary>
+        /// Specifies a Point structure that contains the X- and Y-coordinates of the cursor, in screen coordinates. 
+        /// </summary>
+        public Point Point;
+        /// <summary>
+        /// If the message is WM_MOUSEWHEEL, the high-order word of this member is the wheel delta. 
+        /// The low-order word is reserved. A positive value indicates that the wheel was rotated forward, 
+        /// away from the user; a negative value indicates that the wheel was rotated backward, toward the user. 
+        /// One wheel click is defined as WHEEL_DELTA, which is 120. 
+        ///If the message is WM_XBUTTONDOWN, WM_XBUTTONUP, WM_XBUTTONDBLCLK, WM_NCXBUTTONDOWN, WM_NCXBUTTONUP,
+        /// or WM_NCXBUTTONDBLCLK, the high-order word specifies which X button was pressed or released, 
+        /// and the low-order word is reserved. This value can be one or more of the following values. Otherwise, MouseData is not used. 
+        ///XBUTTON1
+        ///The first X button was pressed or released.
+        ///XBUTTON2
+        ///The second X button was pressed or released.
+        /// </summary>
+        public int MouseData;
+        /// <summary>
+        /// Specifies the event-injected flag. An application can use the following value to test the mouse Flags. Value Purpose 
+        ///LLMHF_INJECTED Test the event-injected flag.  
+        ///0
+        ///Specifies whether the event was injected. The value is 1 if the event was injected; otherwise, it is 0.
+        ///1-15
+        ///Reserved.
+        /// </summary>
+        public int Flags;
+        /// <summary>
+        /// Specifies the Time stamp for this message.
+        /// </summary>
+        public int Time;
+        /// <summary>
+        /// Specifies extra information associated with the message. 
+        /// </summary>
+        public int ExtraInfo;
+    }
+}
---
+++ Gma.GlobalMouseKeyHooks/Gma.UserActivityMonitor/WinApi/Point.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Gma.UserActivityMonitor.WinApi
+{
+    /// <summary>
+    /// The Point structure defines the X- and Y- coordinates of a point. 
+    /// </summary>
+    /// <remarks>
+    /// http://msdn.microsoft.com/library/default.asp?url=/library/en-us/gdi/rectangl_0tiq.asp
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct Point {
+        /// <summary>
+        /// Specifies the X-coordinate of the point. 
+        /// </summary>
+        public int X;
+        /// <summary>
+        /// Specifies the Y-coordinate of the point. 
+        /// </summary>
+        public int Y;
+    }
+}
