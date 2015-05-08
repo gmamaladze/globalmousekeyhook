10ae6f84f0c200eaa15815ff740eb7d17f6577a0
gmamaladze <gmamaladze@gmail.com>
2015-05-09 01:05:53 +0200

    Fixed #11 Prevent UnhookWindowsHookEx calls during application exit.

--- MouseKeyHook/WinApi/HookProcedureHandle.cs
+++ MouseKeyHook/WinApi/HookProcedureHandle.cs
@@ -2,19 +2,31 @@
 // Copyright (c) 2015 George Mamaladze
 // See license.txt or http://opensource.org/licenses/mit-license.php
 
+using System;
+using System.Threading;
 using Microsoft.Win32.SafeHandles;
 
 namespace Gma.System.MouseKeyHook.WinApi
 {
     internal class HookProcedureHandle : SafeHandleZeroOrMinusOneIsInvalid
     {
+        private static bool _closing = false;
+
+        ~HookProcedureHandle()
+        {
+            AppDomain.CurrentDomain.ProcessExit += (sender, e) => {_closing = true;};
+        }
+
         public HookProcedureHandle()
             : base(true)
         {
+            
         }
 
         protected override bool ReleaseHandle()
         {
+            //NOTE Calling Unhook during processexit causes deley
+            if (_closing) return true;
             return HookNativeMethods.UnhookWindowsHookEx(handle) != 0;
         }
     }
