53ffe07c90a1a79e74cc76536b8ec1abbd0a8a48
George Mamaladze <gmamaladze@gmail.com>
2018-01-22 15:17:07 +0100

    Implemented combination detection.

---
+++ MouseKeyHook/KeyCombinationExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook.Implementation;
+
+namespace Gma.System.MouseKeyHook
+{
+    /// <summary>
+    /// Extension methods to detect key combinations
+    /// </summary>
+    public static class KeyCombinationExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="map"></param>
+        public static void OnCombination(this IKeyboardEvents source, IEnumerable<KeyValuePair<Chord, Action>> map)
+        {
+            var watchlists = new HashSet<Keys>(map.SelectMany(p=>p.Key));
+            source.KeyDown += (sender, e) =>
+            {
+                if (!watchlists.Contains(e.KeyCode)) return;
+                var state = KeyboardState.GetCurrent();
+                var matches = map.Where(pair => state.AreAllDown(pair.Key));
+                foreach (var  current in matches)
+                {
+                    current.Value();
+                }
+            };
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="map"></param>
+        public static void OnCombination(this IKeyboardEvents source, IEnumerable<KeyValuePair<TriggerChord, Action>> map)
+        {
+            var watchlists = new HashSet<Keys>(map.Select(p => p.Key.TriggerKey));
+            source.KeyDown += (sender, e) =>
+            {
+                if (!watchlists.Contains(e.KeyCode)) return;
+                var state = KeyboardState.GetCurrent();
+                var matches = map.Where(pair => state.AreAllDown(pair.Key));
+                foreach (var current in matches)
+                {
+                    current.Value();
+                }
+            };
+        }
+    }
+}
