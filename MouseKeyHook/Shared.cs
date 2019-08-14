5358f8cc06cd1ff67e9f813248ff9c9175fd1f2d
FatherFoxxy <rattusunison@gmail.com>
2019-08-13 19:11:39 -0700

    Consolidate FromString logic

---
+++ MouseKeyHook/Shared.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using static Gma.System.MouseKeyHook.WinApi.KeyboardNativeMethods;
+
+namespace Gma.System.MouseKeyHook
+{
+    internal static class Shared
+    {        
+        public static Stack<Keys> FromString(string str)
+        {
+            IEnumerable<Keys> keys =
+                str
+                    .Split('+')
+                    .Select(p =>
+                    {
+                        if (p.Length == 1 && TryGetKeyFromChar(p.First(), out Keys key))
+                            return key;
+                        else
+                            return (Keys)Enum.Parse(typeof(Keys), p, false);
+
+                    });
+            return new Stack<Keys>(keys);
+        }
+    }
+}
