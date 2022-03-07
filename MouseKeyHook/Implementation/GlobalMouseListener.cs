b93ef1164a30f829bfa8f85c2f9aee211bca811c
MarWelz <98609508+MarWelz@users.noreply.github.com>
2022-03-07 14:20:54 +0100

    Update GlobalMouseListener.cs
    Forgot to add a Uses clause.

--- MouseKeyHook/Implementation/GlobalMouseListener.cs
+++ MouseKeyHook/Implementation/GlobalMouseListener.cs
@@ -2,6 +2,7 @@
 // Copyright (c) 2015 George Mamaladze
 // See license.txt or http://opensource.org/licenses/mit-license.php
 
+using System;
 using System.Windows.Forms;
 using Gma.System.MouseKeyHook.WinApi;
 
